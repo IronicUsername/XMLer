920ffd525cc4d6075f7431533fe4209ec3bf949a
IronicUsername <deniztoma512@gmail.com>
2020-10-06 11:00:43 +0200

    flake, mypy, pydoc

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from typing import Any, Dict, IO, Iterator, Optional
+from typing import IO, Iterator, List, Optional
 
 import click
 from helper import create_payment
@@ -8,7 +8,7 @@ from helper import create_payment
 # to do generate sepa xml
 
 
-def _single_sepa_payment(input_file: IO[str]) -> Iterator[Dict[str, Any]]:
+def _single_csv_row(input_file: IO[str]) -> Iterator[List[str]]:
     """Load payments from the input_file, allowing a single json or one json per line."""
     payments = 0
     temp = ''
@@ -18,8 +18,8 @@ def _single_sepa_payment(input_file: IO[str]) -> Iterator[Dict[str, Any]]:
             payment = temp
             temp = ''
             payments += 1
-            payment = payment[:-1].split(';')
-            yield payment
+            res = payment[:-1].split(';')
+            yield res
         except Exception:
             pass
 
@@ -28,17 +28,27 @@ def _single_sepa_payment(input_file: IO[str]) -> Iterator[Dict[str, Any]]:
 @click.argument('input_file', type=click.File('r'))
 @click.option('-o', '--output-path', type=click.Path(exists=True), help='Location where files are saved to.')
 @click.option('--header/--no-header', default=True, help='Print headers as well. Default is yes. Only applies for csv output.')
-def cli(input_file: IO[str], output_path: Optional[str], header: bool):
-    """this scirpt does stuff.
+def cli(input_file: IO[str], output_path: Optional[str], header: bool) -> None:
+    """Script handeler for xml conversion.
+
+    Parameters
+    ----------
+    input_file: IO[str]
+        Path to the input CSV file.
+    output_path: Optional[str]
+        Path to an exising output directory.
+    header: bool
+        Wether a CSV file contains the table header or not.
+        Default is 'True'.
     """
     if output_path is not None and not os.path.isdir(output_path):
-        print('`output_path` has to exist and  be a directory', file=sys.stderr)
+        click.echo('`output_path` has to exist and  be a directory', file=sys.stderr)
         raise click.Abort()
 
     if header:
         next(input_file)
 
-    for payment in _single_sepa_payment(input_file):
+    for payment in _single_csv_row(input_file):
         sepa = create_payment(payment)
         print(sepa.export(validate=True))
 
--- src/helper.py
+++ src/helper.py
@@ -1,29 +1,50 @@
 import datetime
 import json
 import os
-import re
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 
-import chardet
 from sepaxml import SepaDD, SepaTransfer
 
 
-def create_payment(payment: Dict[str, Any]):
-    sepa = ''
+def create_payment(payment: List[str]) -> Any:
+    """Lorem ipsum dolor.
+
+    Parameters
+    ----------
+    payment: List[str]
+        CSV data row.
+
+    Returns
+    -------
+    sepa: Any:
+        Lorem ipsum dolor..
+    """
     config = json.load(open(_base_path() + '/data/config.json', 'r'))
-    payment, p_type = _pack_data(payment)
+    p, p_type = _pack_data(payment)
 
     if p_type == 'debit':
         sepa = SepaDD(config, schema='pain.008.002.02', clean=True)
     elif p_type == 'credit':
         sepa = SepaTransfer(config, clean=True)
 
-    sepa.add_payment(payment)
+    sepa.add_payment(p)
 
     return sepa
 
 
-def _pack_data(payment: List[str]) -> Dict[str, Any]:
+def _pack_data(payment: List[str]) -> Tuple[Dict[str, Any], str]:
+    """Create a payment object that can be handeld by the sepaxml lib.
+
+    Parameters
+    ----------
+    payment: List[str]
+        CSV data row.
+
+    Returns
+    -------
+    ret : Tuple[Dict[str, Any], str]:
+        Contains finished payment object and payment type.
+    """
     p_type = ''
     payment = _sanatize_data(payment)
     res = {'name': payment[0],
@@ -32,42 +53,46 @@ def _pack_data(payment: List[str]) -> Dict[str, Any]:
            'description': payment[4]}
 
     # decide if credit or debit
-    if payment[3] < 0:
+    if int(payment[3]) < 0:
         p_type = 'debit'
 
-        res['amount'] = int(str(payment[3])[1:])
+        res['amount'] = int(payment[3][1:])  # type: ignore
         res['type'] = 'RCUR'
-        res['collection_date'] = datetime.date.today()
+        res['collection_date'] = datetime.date.today()  # type: ignore
         res['mandate_id'] = payment[7]
-        res['mandate_date'] = datetime.date.today()
+        res['mandate_date'] = datetime.date.today()  # type: ignore
 
     else:
         p_type = 'credit'
 
-        res['amount'] = payment[3]
-        res['execution_date'] = datetime.date.today()
+        res['amount'] = int(payment[3])  # type: ignore
+        res['execution_date'] = datetime.date.today()  # type: ignore
 
-    return res, p_type
+    ret = (res, p_type)
+    return ret
 
 
 def _base_path() -> str:
+    """Path to project root."""
     return os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 
-def _get_encoding(file_path: str) -> str:
-    f = chardet.detect(open(file_path, 'rb').read())
-    res = f['encoding'] if f['confidence'] >= 0.5 else 'utf-8'
-
-    return res
-
-
 def _sanatize_data(data: List[str]) -> List[str]:
-    # money amount
-    data[3] = ''.join(data[3].split(','))
-    data[3] = int(''.join(data[3].split('.')))
+    """Remove 'bad stuff' from csv dataset.
 
-    # mandate ID
-    data[7] = int(re.search(r'\d+', data[7]).group())
+    Parameters
+    ----------
+    data: List[str]
+        CSV data row.
+
+    Returns
+    -------
+    data: List[str]:
+        Ready-to-go CSV data row.
+    """
+    # money amount
+    bad_chars = [',', '.']
+    data[3] = data[3].translate({ord(x): '' for x in bad_chars})
 
     # create description
     data[4] = f'{data[4]} {data[5]} {data[6]}'
